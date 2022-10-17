1174ec8f12a666180fe322b1231e264f0647ee0b
joseluizs <joseluiz.smarques@gmail.com>
2022-10-17 19:11:18 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/joseluiz/workshopmongo/services/UserService.java
+++ src/main/java/com/joseluiz/workshopmongo/services/UserService.java
@@ -13,25 +13,32 @@ import com.joseluiz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void deleteById(String id) {
+
+		if (!(findById(id) == null)) {
+			repo.deleteById(id);
+		}
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
+
 }
