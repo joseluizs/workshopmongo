24665f7a441767e04bfff90f0f4177b762a9c426
joseluizs <joseluiz.smarques@gmail.com>
2022-10-17 20:25:49 -0300

    Referenciando os posts do usuário

--- src/main/java/com/joseluiz/workshopmongo/config/Instatiation.java
+++ src/main/java/com/joseluiz/workshopmongo/config/Instatiation.java
@@ -37,10 +37,12 @@ public class Instatiation implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hj!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
