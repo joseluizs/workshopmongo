7564ab41bf381174d196e35b98ee680ea0e03400
joseluizs <joseluiz.smarques@gmail.com>
2022-10-17 21:25:28 -0300

    Consulta simples com query methods

--- src/main/java/com/joseluiz/workshopmongo/services/PostService.java
+++ src/main/java/com/joseluiz/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.joseluiz.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 		Optional<Post> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	} 
 
 }
